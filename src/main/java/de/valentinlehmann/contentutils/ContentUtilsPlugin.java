fe39ec559d95870a2b1a5bc3d301ad62c190e9c7
Valentin Lehmann <info@valentinlehmann.de>
2021-07-30 22:45:35 +0200

    feat: added plugin.yml and main class

---
+++ src/main/java/de/valentinlehmann/contentutils/ContentUtilsPlugin.java
@@ -0,0 +1,15 @@
+package de.valentinlehmann.contentutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ContentUtilsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
