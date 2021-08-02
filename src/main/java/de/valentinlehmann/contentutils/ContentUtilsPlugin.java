3817395df7692a87e308f9da7663fb862afaef3a
Valentin Lehmann <info@valentinlehmann.de>
2021-08-02 18:44:59 +0200

    feat: added localize system and basic aikar features

--- src/main/java/de/valentinlehmann/contentutils/ContentUtilsPlugin.java
+++ src/main/java/de/valentinlehmann/contentutils/ContentUtilsPlugin.java
@@ -1,15 +1,23 @@
 package de.valentinlehmann.contentutils;
 
+import co.aikar.commands.PaperCommandManager;
+import de.valentinlehmann.contentutils.command.impl.SoundCommand;
+import de.valentinlehmann.contentutils.utils.LocalizeUtils;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
 public class ContentUtilsPlugin extends JavaPlugin {
+    private final LocalizeUtils localizeUtils = new LocalizeUtils(this);
+    private PaperCommandManager commandManager;
+
     @Override
     public void onEnable() {
         super.onEnable();
-    }
 
-    @Override
-    public void onDisable() {
-        super.onDisable();
+        this.commandManager = new PaperCommandManager(this);
+        this.commandManager.registerCommand(new SoundCommand(this));
+
+        this.localizeUtils.loadMessages();
     }
 }
