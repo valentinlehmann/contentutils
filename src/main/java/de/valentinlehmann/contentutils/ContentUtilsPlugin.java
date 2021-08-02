a87efe8f88b4b476ec1af56566ca63da2e8f10cd
Valentin Lehmann <info@valentinlehmann.de>
2021-08-02 18:59:49 +0200

    feat: implemented class path utils and moved command and listener registration

--- src/main/java/de/valentinlehmann/contentutils/ContentUtilsPlugin.java
+++ src/main/java/de/valentinlehmann/contentutils/ContentUtilsPlugin.java
@@ -1,13 +1,17 @@
 package de.valentinlehmann.contentutils;
 
 import co.aikar.commands.PaperCommandManager;
-import de.valentinlehmann.contentutils.command.impl.SoundCommand;
+import de.valentinlehmann.contentutils.command.AbstractCommand;
+import de.valentinlehmann.contentutils.listener.AbstractListener;
+import de.valentinlehmann.contentutils.utils.ClassPathUtils;
 import de.valentinlehmann.contentutils.utils.LocalizeUtils;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 public class ContentUtilsPlugin extends JavaPlugin {
+    private final ClassPathUtils classPathUtils = new ClassPathUtils(this);
     private final LocalizeUtils localizeUtils = new LocalizeUtils(this);
     private PaperCommandManager commandManager;
 
@@ -16,7 +20,11 @@ public class ContentUtilsPlugin extends JavaPlugin {
         super.onEnable();
 
         this.commandManager = new PaperCommandManager(this);
-        this.commandManager.registerCommand(new SoundCommand(this));
+        this.classPathUtils.createInstanceAndApplyAction("de.valentinlehmann.contentutils.command.impl",
+                AbstractCommand.class, command -> this.commandManager.registerCommand(command), this);
+
+        this.classPathUtils.createInstanceAndApplyAction("de.valentinlehmann.contentutils.listener.impl",
+                AbstractListener.class, listener -> Bukkit.getPluginManager().registerEvents(listener, this), this);
 
         this.localizeUtils.loadMessages();
     }
---
+++ src/main/java/de/valentinlehmann/contentutils/utils/ClassPathUtils.java
@@ -0,0 +1,36 @@
+package de.valentinlehmann.contentutils.utils;
+
+import de.valentinlehmann.contentutils.ContentUtilsPlugin;
+import lombok.RequiredArgsConstructor;
+import org.reflections.Reflections;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+public class ClassPathUtils {
+    private final ContentUtilsPlugin plugin;
+
+    private <T> Set<Class<? extends T>> getAllClassesInPackage(String packageName, Class<T> type) {
+        Reflections reflections = new Reflections(packageName);
+        return reflections.getSubTypesOf(type);
+    }
+
+    public <T> void createInstanceAndApplyAction(String packageName, Class<T> type, Consumer<T> action, Object... constructorArgs) {
+        Class<?>[] constructorArgTypes = new Class[constructorArgs.length];
+
+        for (int i = 0; i < constructorArgs.length; i++) {
+            constructorArgTypes[i] = constructorArgs[i].getClass();
+        }
+
+        getAllClassesInPackage(packageName, type).forEach(clazz -> {
+            try {
+                action.accept(clazz.getConstructor(constructorArgTypes).newInstance(constructorArgs));
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                this.plugin.getLogger().severe(String.format("Konnte Klassen im Package %s nicht registrieren! Fehler:", packageName));
+                e.printStackTrace();
+            }
+        });
+    }
+}
