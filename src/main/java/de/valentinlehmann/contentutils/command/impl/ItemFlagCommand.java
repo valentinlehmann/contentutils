61c17292228987654fba43b30d2b8b888b4c9fbe
Valentin Lehmann <info@valentinlehmann.de>
2021-08-03 13:29:10 +0200

    feat #3: added itemflags alias for itemflag

--- src/main/java/de/valentinlehmann/contentutils/command/impl/ItemFlagCommand.java
+++ src/main/java/de/valentinlehmann/contentutils/command/impl/ItemFlagCommand.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @CommandPermission("contentutils.itemflag")
-@CommandAlias("itemflag")
+@CommandAlias("itemflag|itemflags")
 public class ItemFlagCommand extends AbstractCommand {
     public ItemFlagCommand(ContentUtilsPlugin plugin) {
         super(plugin);
