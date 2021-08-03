bc1397b8353396600d6309d52f98e9d31f332142
Valentin Lehmann <info@valentinlehmann.de>
2021-08-03 02:41:26 +0200

    feat: added lore command

--- src/main/java/de/valentinlehmann/contentutils/ContentUtilsPlugin.java
+++ src/main/java/de/valentinlehmann/contentutils/ContentUtilsPlugin.java
@@ -1,5 +1,6 @@
 package de.valentinlehmann.contentutils;
 
+import co.aikar.commands.ConditionFailedException;
 import co.aikar.commands.PaperCommandManager;
 import de.valentinlehmann.contentutils.command.AbstractCommand;
 import de.valentinlehmann.contentutils.core.inventory.InventoryRepository;
@@ -9,6 +10,8 @@ import de.valentinlehmann.contentutils.utils.ClassPathUtils;
 import de.valentinlehmann.contentutils.utils.LocalizeUtils;
 import lombok.Getter;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
@@ -25,9 +28,19 @@ public class ContentUtilsPlugin extends JavaPlugin {
 
         this.commandManager = new PaperCommandManager(this);
         this.commandManager.enableUnstableAPI("help");
+        this.commandManager.getCommandConditions().addCondition("item_in_hand", context -> {
+            if (context.getIssuer().getPlayer().getItemInHand() != null
+                    && context.getIssuer().getPlayer().getItemInHand().getType() != Material.AIR) {
+                return;
+            }
+
+            throw new ConditionFailedException(this.localizeUtils.getMessage("contentutils.command.no-item-in-hand"));
+        });
+        this.commandManager.getCommandContexts().registerOptionalContext(ItemStack.class,
+                context -> context.getPlayer().getItemInHand());
+
         this.classPathUtils.createInstanceAndApplyAction("de.valentinlehmann.contentutils.command.impl",
                 AbstractCommand.class, command -> this.commandManager.registerCommand(command), this);
-
         this.classPathUtils.createInstanceAndApplyAction("de.valentinlehmann.contentutils.listener.impl",
                 AbstractListener.class, listener -> Bukkit.getPluginManager().registerEvents(listener, this), this);
 
